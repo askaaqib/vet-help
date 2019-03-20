a4d9e97642c920733d7b797b71f54d41719d7008
hassanshuja <shahogjalal@gmail.com>
2019-03-20 19:53:57 +0500

    react-init

---
+++ mern/vet-backend/routes/vet.server.route.js
@@ -0,0 +1,24 @@
+// ./express-server/routes/todo.server.route.js
+import express from 'express';
+//import controller file
+import passport from 'passport';
+// import * as todoController from '../controllers/vet.server.controller';
+import * as UserController from '../controllers/user';
+// get an instance of express router
+
+const router = express.Router();
+// router.route('/')
+//      .get(todoController.getTodos)
+//      .post(todoController.addTodo)
+//      .put(todoController.updateTodo);
+// router.route('/:id')
+//       .get(todoController.getTodo)
+//       .delete(todoController.deleteTodo);
+router.route('/register')
+      .post(UserController.register);
+router.route('/login')
+      .post(UserController.login);
+router.route('/me')
+      .get(passport.authenticate('jwt', { session: false }), UserController.me);
+
+export default router;
